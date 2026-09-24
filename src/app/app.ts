57cc81e40886adb2f73e16196f8319013461b34d
dev <dev@local>
2026-09-24 15:23:55 +0530

    Fix in-page section links and make the header actually sticky

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { Footer } from './layout/footer/footer';
 import { Header } from './layout/header/header';
 
+/** Height of the sticky header plus the service-page sub-navigation bar. */
+const STICKY_OFFSET = 132;
+
 @Component({
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, RouterLink, Header, Footer],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
 })
-export class App {}
+export class App {
+  constructor() {
+    // Router anchor scrolling uses window.scrollTo, which ignores CSS scroll-margin.
+    inject(ViewportScroller).setOffset([0, STICKY_OFFSET]);
+  }
+}
